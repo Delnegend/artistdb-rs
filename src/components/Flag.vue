7ad0a893480aaefb0ddf715c3f1d36c627cb4ced
Delnegend <kiennguyen19323@gmail.com>
2024-02-22 12:35:23 +0700

    feat: :sparkles: restructure nuxt to src dir; finalize the frontend; add noto-sans; rm social icons

---
+++ src/components/Flag.vue
@@ -0,0 +1,5 @@
+<template>
+	<span style="font-family: &quot;Twemoji Country Flags&quot;">
+		<slot />
+	</span>
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+	<div
+		class="flex h-[100vh] w-full flex-col items-center justify-center bg-black text-white/90"
+	>
+		<div class="text-6xl">Hi!</div>
+		<a
+			class="text-2xl text-indigo-500"
+			href="https://github.com/Delnegend/artist-db"
+			>Delnegend/artist-db</a
+		>
+	</div>
+</template>
