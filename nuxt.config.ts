f6d9a6c462b56f763a7cb21b1689eaea94682110
Delnegend <kiennguyen19323@gmail.com>
2024-02-22 12:34:16 +0700

    build: :package: more plugins; eslint ignore + tailwind; nuxt config; gitignore; scripts
    - .gitignore: add src/composables/bincode-2-artist-wasm
    - eslint rule: allow custom classname beside tailwindcss
    - eslintignore: bincode-2-artist-wasm
    - nuxt: change srcDir from root (default) to src, making room for src-rust
    - nuxt plugin: topLevelAwait
    - pkg.json scripts: dev now include artist-2-bincode, generate now generate bincode first, change
    path for build:wasm and build:tool
    - pkg.json dep: country flag emoji polyfill

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineNuxtConfig({
+	srcDir: "src",
 	ssr: false,
 	app: {
 		baseURL: "/",
@@ -13,6 +15,19 @@ export default defineNuxtConfig({
 	eslint: { lintOnStart: false },
 	experimental: { viewTransition: true },
 	vite: {
-		plugins: [wasm()]
-	}
+		plugins: [
+			wasm(),
+			topLevelAwait({
+				// The export name of top-level await promise for each chunk module
+				promiseExportName: "__tla",
+				// The function to generate import names of top-level await promise in each chunk module
+				promiseImportName: i => `__tla_${i}`
+			})
+		]
+	},
+	nitro: {
+		experimental: {
+			wasm: true,
+		},
+	},
 });
