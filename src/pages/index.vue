b127a767e8b5deb18b869a8df6a023c3fabc3fc4
Delnegend <kiennguyen19323@gmail.com>
2024-03-01 21:50:13 +0700

    style: :lipstick: lint

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,11 +1,7 @@
 <template>
-	<div
-		class="flex h-[100vh] w-full flex-col items-center justify-center bg-black text-white/90"
-	>
+	<div class="flex h-[100vh] w-full flex-col items-center justify-center bg-black text-white/90">
 		<div class="text-6xl">Hi!</div>
-		<a
-			class="text-2xl text-indigo-500"
-			href="https://github.com/Delnegend/artist-db"
+		<a class="text-2xl text-indigo-500" href="https://github.com/Delnegend/artist-db"
 			>Delnegend/artist-db</a
 		>
 	</div>
