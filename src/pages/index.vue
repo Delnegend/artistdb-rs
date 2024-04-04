4bf258bc73a5fc4eeac20d1526f7fc86cf56d9f5
Delnegend <kiennguyen19323@gmail.com>
2024-04-04 17:15:26 +0700

    refactor(index.vue): :recycle: noto serif display for the title

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,8 +1,14 @@
 <template>
 	<div class="flex h-screen w-full flex-col items-center justify-center bg-black text-white/90">
-		<div class="text-6xl">Hi!</div>
+		<div class="text-6xl serif-display">Hi!</div>
 		<a class="text-2xl text-indigo-500" href="https://github.com/Delnegend/artist-db"
 			>Delnegend/artist-db</a
 		>
 	</div>
 </template>
+
+<style scoped>
+.serif-display {
+	font-family: "Noto Serif Display", sans-serif;
+}
+</style>
