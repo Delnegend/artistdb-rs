45f30ea9cdb5626ceaf52fcebc2d6a24abbcdf8b
Delnegend <kiennguyen19323@gmail.com>
2024-04-05 17:56:44 +0700

    fix(fetchUserInfo.ts): :bug: handle response not ok & nuxt's custom 404 page

--- src/composables/fetchUserInfo.ts
+++ src/composables/fetchUserInfo.ts
@@ -1,7 +1,13 @@
 export async function fetchUserInfo(username: string): Promise<string> {
     try {
         const res = await fetch(`/artists/${username}`);
+        if (!res.ok) {
+            return new Promise((_, reject) => reject("error"));
+        }
         const content = await res.text();
+        if (content.startsWith("<")) {
+            return new Promise((_, reject) => reject("error"));
+        }
         if (content.startsWith("@")) {
             const res = await fetch(`/artists/${content.slice(1)}`);
             return await res.text();
