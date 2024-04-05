4af3e518ed5ee8f787b69baa1539ffc0683f955b
Delnegend <kiennguyen19323@gmail.com>
2024-04-05 17:55:57 +0700

    style: :lipstick: lint

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,7 +4,7 @@ document.title = `ArtistDB`;
 
 <template>
 	<div class="flex h-screen w-full flex-col items-center justify-center bg-black text-white/90">
-		<div class="text-6xl serif-display">Hi!</div>
+		<div class="serif-display text-6xl">Hi!</div>
 		<a class="text-2xl text-indigo-500" href="https://github.com/Delnegend/artist-db"
 			>Delnegend/artist-db</a
 		>
